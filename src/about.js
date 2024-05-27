02bc75faf1243158bf1148de9b4221d1eda2eefa
Tim Wallace <timwallacedev@gmail.com>
2024-05-27 15:48:15 -0400

    update projects

--- src/about.js
+++ src/about.js
@@ -2,11 +2,7 @@ function About() {
     return (
         <div id="about">
             <p id="about-copy">
-                Hello, I'm Tim Wallace, a web developer with a love for coding, bikes, photography, and music—especially playing the trombone. I discovered programming during the COVID-19 pandemic while creating a website for my band. Raised in vibrant Toronto, this marked a new chapter in my exploration of web development.
-                Crafting a digital space for my band became a source of solace and inspiration, showcasing our music and sparking a broader passion for coding. Away from the screen, I find joy in cycling through Toronto's streets or on scenic trails, balancing the digital with the freedom of the open road.
-                Photography is another passion, allowing me to capture the beauty of the world with my keen eye. Whether it's urban landscapes or the great outdoors, my photographs tell stories without words.
-                When not immersed in the digital realm or exploring through my camera lens, you'll find me in the world of music, often playing the trombone. Whether experimenting with genres or attending live performances, music is a constant inspiration.
-                In a world where technology and creativity converge, my journey is a harmonious blend of coding, music, cycling, and photography—a testament to the transformative power of passion where the digital and analog seamlessly come together.
+            Hello, I'm Tim Wallace, a web developer with a love for coding, bikes, photography, and music. I discovered programming during the COVID-19 pandemic while creating a website for my band which marked a new chapter in my exploration of web development. When not immersed in coding, you'll find me in the world of music, often playing the trombone.
             </p>
         </div>
     )
--- src/projects.js
+++ src/projects.js
@@ -1,41 +1,31 @@
+import { Link } from "react-router-dom";
+
+
 function Projects() {
 
-    const handleClick1 = () => {
-        // Replace the following line with the desired behavior when the div is clicked
-        window.location.href = 'https://humanmagic.ca';
-    };
-    const handleClick2 = () => {
-        // Replace the following line with the desired behavior when the div is clicked
-        window.location.href = 'https://shouttheband.com';
-    };
-    const handleClick3 = () => {
-        // Replace the following line with the desired behavior when the div is clicked
-        window.location.href = 'http://ourhomeapp.com/';
-    };
     return (
         <div id="projects">
             <h3 id="projects-heading">Projects</h3>
 
             <div id="projects-container" >
-                <div class="project" onClick={handleClick1}>
-                    <h4 class="project-title">Human Magic</h4>
-                    <p class="project-description">A Website for local Toronto band "Human Magic".</p>
-                    <img src=""></img>
-                </div>
 
-                <div class="project" onClick={handleClick2}>
-                    <h4 class="project-title">Shout! The Band</h4>
-                    <p class="project-description">A Website for local Toronto band "Shout! The Band".</p>
-                    <img src=""></img>
-                </div>
+                <a href="https://timwallace.ca/melody-mastermind/#/melody-mastermind">
+                    <div className="project">
+                        <h4 className="project-title">Melody Mastermind</h4>
+                        <p className="project-description">A music guessing game.</p>
+                    </div>
+                </a>
 
-                <div class="project" onClick={handleClick3}>
-                    <h4 class="project-title">Chores Minder</h4>
-                    <p class="project-description">A website for the whole family/roomates to remember their chores! (I didn't make this site).</p>
-                    <img src=""></img>
-                </div>
+                <a href="https://humanmagic.ca">
+                    <div className="project">
+                        <h4 className="project-title">Human Magic</h4>
+                        <p className="project-description">A Website for local Toronto band "Human Magic".</p>
+                    </div>
+                </a>
+
+
             </div>
-        </div>
+        </div >
     )
 }
 
