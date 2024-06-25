fcfadae7e9e9098e9c972a2ead54b3b882a8b165
Tim Wallace <timwallacedev@gmail.com>
2024-06-25 17:32:15 -0400

    add resume pdf

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -3,12 +3,13 @@ import "./Resume.scss"
 import download from "../../assets/download.svg"
 import downloadHover from "../../assets/download--hover.svg"
 import { useState } from 'react'
+import resume from "../../assets/Tim Wallace - Resume.pdf"
 
 function Resume() {
 
     const [isHovering, setIsHovering] = useState(false)
 
-    function hover(){
+    function hover() {
         setIsHovering(true)
     }
 
@@ -17,10 +18,12 @@ function Resume() {
     }
 
     return (
-        <div className="resume" onMouseEnter={hover} onMouseLeave={dehover}>
-            <img src={isHovering? downloadHover : download} alt="download icon" className="resume__download-icon" />
-            <span className="resume__text">Resume</span>
-        </div>
+        <a href={resume} download>
+            <div className="resume" onMouseEnter={hover} onMouseLeave={dehover}>
+                <img src={isHovering ? downloadHover : download} alt="download icon" className="resume__download-icon" />
+                <span className="resume__text">Resume</span>
+            </div>
+        </a>
     )
 }
 
