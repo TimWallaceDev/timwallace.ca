bb3e8d19201e888dbf14f4555c2cc8ecaac368e5
Tim Wallace <timwallacedev@gmail.com>
2024-08-13 08:47:25 -0400

    about copy spacing update

--- src/about.js
+++ src/about.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="about">
             <p className="about__copy">
-            Hello! I'm Tim Wallace, a full-stack developer with a background in jazz trombone.My musical roots have instilled in me the importance of timing, nuance, and simplicity, teaching me that less is often more—a principle I carry into my coding work. I excel at building user-centered websites with modern frameworks like React and Express. When I'm not writing code, I’m on my YouTube channel, where I take on CSS challenges and share my techniques and insights. My passion for both learning and teaching drives me to continuously grow as a developer.
+            Hello! I'm Tim Wallace, a full-stack developer with a background in jazz trombone. My musical roots have instilled in me the importance of timing, nuance, and simplicity, teaching me that less is often more—a principle I carry into my coding work. I excel at building user-centered websites with modern frameworks like React and Express. When I'm not writing code, I’m on my YouTube channel, where I take on CSS challenges and share my techniques and insights. My passion for both learning and teaching drives me to continuously grow as a developer.
             </p>
         </div>
     )
