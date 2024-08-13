c4841836cb5da53b8837677c07a3addc9333f3ba
Tim Wallace <timwallacedev@gmail.com>
2024-08-12 21:24:17 -0400

    add new font

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import "./Resume.scss"
-import download from "../../assets/download.svg"
-import downloadHover from "../../assets/download--hover.svg"
-import { useState } from 'react'
 
 function Resume() {
 
-    const [isHovering, setIsHovering] = useState(false)
-
-    function hover() {
-        setIsHovering(true)
-    }
-
-    function dehover() {
-        setIsHovering(false)
-    }
-
     return (
         <a href={`${process.env.PUBLIC_URL}/Tim Wallace - Resume.pdf`} download>
-            <div className="resume" onMouseEnter={hover} onMouseLeave={dehover}>
-                <img src={isHovering ? downloadHover : download} alt="download icon" className="resume__download-icon" />
+            <div className="resume">
+                <span className="material-symbols-outlined resume__icon">download</span>
                 <span className="resume__text">Resume</span>
             </div>
         </a>
