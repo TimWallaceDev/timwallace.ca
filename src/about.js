6dc0dec32065d1f1c53b3f4c40821c9c87e47f96
Tim Wallace <timwallacedev@gmail.com>
2024-09-27 10:38:54 -0400

    small UI improvements

--- src/about.js
+++ src/about.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="about">
             <p className="about__copy">
-            Hello! I'm Tim Wallace, a full-stack developer with a background in jazz trombone. My musical roots have taught me the value of timing, nuance, and simplicity—principles I apply in my coding work, where I believe that less is often more. I specialize in building user-centered, performance-driven websites using modern frameworks like React, Express, and TypeScript, with a focus on clean code and intuitive design. My passion for learning drives me to continuously expand my skills in areas like API development, testing, and scalability.
+            Hello! I'm Tim Wallace, a full-stack developer with a background in jazz trombone. My musical roots have taught me the value of <b>timing, nuance, and simplicity</b> — principles I apply in my coding work, where I believe that less is often more. I specialize in building user-centered, performance-driven websites using modern frameworks like React, Express, and TypeScript, with a focus on clean code and intuitive design. My passion for learning drives me to continuously expand my skills in areas like API development, testing, and scalability.
             </p>
         </div>
     )
--- src/hero.js
+++ src/hero.js
@@ -1,11 +1,11 @@
-
+import arrow from "./assets/arrow_forward.svg"
 
 
 const Hero = () => {
     return (
         <div className="hero">
             <h1 className="name">Tim Wallace</h1>
-            <h2 className="occupation">Full Stack Developer</h2>
+            <h2 className="occupation">Musician <img src={arrow}/> Full Stack Developer</h2>
         </div>
     )
 }
