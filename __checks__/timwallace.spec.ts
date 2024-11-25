9520c4d1a17784c54f560cc9f908edb0ba257ee9
Tim Wallace <timwallacedev@gmail.com>
2024-11-24 19:14:16 -0500

    change test name

--- __checks__/timwallace.spec.ts
+++ __checks__/timwallace.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 // You can override the default Playwright test timeout of 30s
 // test.setTimeout(60_000);
 
-test('webshop homepage', async ({ page }) => {
+test('Portfolio homepage', async ({ page }) => {
   const response = await page.goto('https://timwallace.ca')
   expect(response?.status()).toBeLessThan(400)
   await expect(page).toHaveTitle(/Tim Wallace/)
